9d1d17f05ee77510b31a97940bd694fd8a9066b0
talb75575 <talbrachya10@gmail.com>
2024-04-28 23:26:49 +0300

    Final

--- src/main/java/com/att/tdp/bisbis10/repository/CuisineRepository.java
+++
@@ -1,9 +0,0 @@
-package com.att.tdp.bisbis10.repository;
-
-import com.att.tdp.bisbis10.entity.Cuisine;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CuisineRepository extends JpaRepository<Cuisine, Long> {
-    Cuisine findByName(String name);
-}
-
